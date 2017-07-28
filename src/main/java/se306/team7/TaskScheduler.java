f8f895614ae77eb5eb29b63e7f717bdca6d7f4f3
Nathan Kear <n.j.kear@gmail.com>
2017-07-28 12:55:47 +1200

    Add logging using sl4j and log4j

--- src/main/java/se306/team7/TaskScheduler.java
+++ src/main/java/se306/team7/TaskScheduler.java
@@ -1,5 +1,9 @@
 package se306.team7;
 
+import org.apache.log4j.PropertyConfigurator;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
@@ -8,6 +12,14 @@ public class TaskScheduler
 {
     public static void main( String[] args )
     {
+        //BasicConfigurator.configure();
+        PropertyConfigurator.configure("src/log4j.properties");
+        Logger logger = LoggerFactory.getLogger(TaskScheduler.class);
+        logger.trace("trace message");
+        logger.debug("debug message");
+        logger.info("info message");
+        logger.warn("warn message");
+        logger.error("error message");
         System.out.println( "Hello World!" );
     }
 }
