0e68dd33de1c015aeaad47eab3e5082ba9c19474
Nathan Kear <n.j.kear@gmail.com>
2017-08-04 01:15:50 +1200

    Add CommandLineArgumentParser to parse input args to CommandLineArgumentConfig object

---
+++ src/main/java/se306/team7/ICommandLineArgumentParser.java
@@ -0,0 +1,10 @@
+package se306.team7;
+
+public interface ICommandLineArgumentParser {
+    /**
+     * Parse the given command line arguments
+     * @param args
+     * @return Object containing the program configuration specified by the given command line arguments
+     */
+    CommandLineArgumentConfig ParseCommandLineArguments(String[] args);
+}
