20b4def6c04b2728bcecb5b922fdd91f0c2b305c
Nathan Kear <n.j.kear@gmail.com>
2017-08-04 01:18:00 +1200

    Allow application to intepret input args and load input file

--- src/main/java/se306/team7/TaskScheduler.java
+++ src/main/java/se306/team7/TaskScheduler.java
@@ -3,6 +3,13 @@ package se306.team7;
 import org.apache.log4j.PropertyConfigurator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import se306.team7.utility.FileUtilities;
+import se306.team7.utility.IFileUtilities;
+
+import java.io.*;
+
+
+
 
 /**
  * Hello world!
@@ -10,16 +17,67 @@ import org.slf4j.LoggerFactory;
  */
 public class TaskScheduler
 {
+    private static IFileUtilities fileUtilities = new FileUtilities();
+    private static ICommandLineArgumentParser commandLineArgumentParser = new CommandLineArgumentParser(fileUtilities);
+    private static Logger logger = LoggerFactory.getLogger(TaskScheduler.class);
+
     public static void main( String[] args )
     {
-        //BasicConfigurator.configure();
         PropertyConfigurator.configure("src/log4j.properties");
-        Logger logger = LoggerFactory.getLogger(TaskScheduler.class);
+
+        CommandLineArgumentConfig commandLineArgumentConfig;
+
+        try {
+            commandLineArgumentConfig = commandLineArgumentParser.ParseCommandLineArguments(args);
+        } catch (CommandLineArgumentException ex) {
+            System.err.println(ex.getMessage());
+            logger.error(ex.getMessage());
+            printCommandFormatInfo();
+            System.exit(1);
+        }
+
         logger.trace("trace message");
         logger.debug("debug message");
         logger.info("info message");
         logger.warn("warn message");
         logger.error("error message");
         System.out.println( "Hello World!" );
+    }
+
+    private static void /*Digraph*/ readInputFileToDigraph(String inputFileName) {
+
+        // Digraph digraph = new Digraph();
+
+        try {
+
+            BufferedReader bufferedReader = fileUtilities.CreateFileReader(inputFileName);
+            String inputLine = bufferedReader.readLine();
+
+            while (inputLine != null) {
+                // Send inputLine to DigraphParser
+                // digraph = DigraphParser.ParseLine(inputLine, digraph);
+            }
+
+        } catch (FileNotFoundException ex) {
+            logger.error("Command in incorrect format");
+            System.err.printf("Unable to find file %s", inputFileName);
+            System.exit(1);
+        } catch (IOException ex) {
+            logger.error("Command in incorrect format");
+            System.err.printf("Unable to read file %s", inputFileName);
+            System.exit(1);
+        }
+    }
+
+    private static void printCommandFormatInfo() {
+        System.err.print("Expected command format:\n" +
+                "\n" +
+                "java −jar scheduler.jar INPUT.dot P [OPTION]\n" +
+                "INPUT.dot a task graph with integer weights in dot format\n" +
+                "P number of processors to schedule the INPUT graph on\n" +
+                "Optional :\n" +
+                "− p N use N cores for execution in parallel (default is sequential)\n" +
+                "− v visualise the search\n" +
+                "− o OUPUT output file is named OUTPUT (default is INPUT−output.dot)\n");
     }
 }
