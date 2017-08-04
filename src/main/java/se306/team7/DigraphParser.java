3bc705f23df9e1bd0f8897a6c815d03d7e495523
Max Griffith <mgriffith37@gmail.com>
2017-08-04 16:53:10 +1200

    Modify pom file to change junit version.

--- src/main/java/se306/team7/DigraphParser.java
+++ src/main/java/se306/team7/DigraphParser.java
@@ -2,7 +2,13 @@ package se306.team7;
 
 public class DigraphParser implements DigraphParserInterface{
     public Digraph parseLine(Digraph d, String line){
-        
+        //split string into nonspace characters and non bracket characters
+        //first letter is node
+        //if followed by arrow, must be a link
+        //weight is after
+
+        //have to determine if its a node or link
+        //once thats done, adjust digraph to add node/link
         return null;
     }
 }
