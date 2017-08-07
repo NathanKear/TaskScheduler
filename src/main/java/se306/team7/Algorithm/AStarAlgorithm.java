8d7007dfb1c49a21a0469a74f7aff1ccba64a70c
Nathan Kear <n.j.kear@gmail.com>
2017-08-08 00:57:42 +1200

    Implement generateSchedules

--- src/main/java/se306/team7/Algorithm/AStarAlgorithm.java
+++ src/main/java/se306/team7/Algorithm/AStarAlgorithm.java
@@ -1,5 +1,6 @@
 package se306.team7.Algorithm;
 
+import se306.team7.CostEstimatedSchedule;
 import se306.team7.Digraph.Digraph;
 import se306.team7.Digraph.Node;
 import se306.team7.Schedule;
@@ -12,12 +13,12 @@ import java.util.ArrayList;
 
 public class AStarAlgorithm implements IAlgorithm {
 
-    private PriorityQueue <Schedule> _schedules;
+    private PriorityQueue <CostEstimatedSchedule> _schedules;
     private Map <Schedule, List<Node>> _currentHeads;
     private Digraph _digraph;
 
     public AStarAlgorithm () {
-        _schedules = new PriorityQueue<Schedule>();
+        _schedules = new PriorityQueue<CostEstimatedSchedule>();
         _currentHeads = new HashMap<Schedule, List<Node>>();
     }
 
@@ -26,28 +27,40 @@ public class AStarAlgorithm implements IAlgorithm {
         _currentHeads.clear();
         _digraph = digraph;
 
-        Schedule schedule = new Schedule(numOfProcessors, Integer.MAX_VALUE);
+        Schedule schedule = new Schedule(numOfProcessors);
 
-        _schedules.add(schedule);
+        _schedules.add(new CostEstimatedSchedule(schedule, Integer.MAX_VALUE));
 
         while(true){
-           Schedule mostPromisingSchedule =  _schedules.poll();
+           Schedule mostPromisingSchedule =  _schedules.poll().getSchedule();
            List<Node> possibleNodes = _currentHeads.get(mostPromisingSchedule);
 
            if(possibleNodes.isEmpty()) {
                return mostPromisingSchedule;
            }
 
-           List<Schedule> possibleSchedules = generateSchedules(mostPromisingSchedule, possibleNodes);
+           List<CostEstimatedSchedule> possibleSchedules = generateSchedules(mostPromisingSchedule, possibleNodes);
 
-           for(Schedule s : possibleSchedules){
+           for(CostEstimatedSchedule s : possibleSchedules){
                _schedules.add(s);
            }
         }
     }
 
-    public List<Schedule> generateSchedules(Schedule current, List<Node> currentHeads) {
-        return new ArrayList<Schedule>();
+    public List<CostEstimatedSchedule> generateSchedules(Schedule current, List<Node> currentHeads) {
+
+        List<CostEstimatedSchedule> generatedSchedules = new ArrayList<CostEstimatedSchedule>();
+
+        for (Node head : currentHeads) {
+            for (int i = 0; i < current._numOfProcessors; i++) {
+                Schedule newSchedule = new Schedule(current);
+                newSchedule.scheduleTask(i, head);
+                int cost = getCostEstimate(newSchedule);
+                generatedSchedules.add(new CostEstimatedSchedule(newSchedule, cost));
+            }
+        }
+
+        return generatedSchedules;
     }
 
     public int getCostEstimate(Schedule current) {
