32aae326bf92b9b112dc0e82ac76442d74748555
Charles Carey <ccar845@aucklanduni.ac.nz>
2017-08-08 10:51:42 +1200

    Add constructor and getter and setter for Task

--- src/main/java/se306/team7/Task.java
+++ src/main/java/se306/team7/Task.java
@@ -3,6 +3,21 @@ package se306.team7;
 import se306.team7.Digraph.Node;
 
 public class Task {
-    Node node;
-    int processor;
+
+    private Node _node;
+    private int _processor;
+
+    public Task (Node n, int processor) {
+        _node = n;
+        _processor = processor;
+    }
+
+    public Node getNode () {
+        return _node;
+    }
+
+    public int getProcessor () {
+        return _processor;
+    }
+
 }
