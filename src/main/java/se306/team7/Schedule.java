b9f0c5ff16b174fc16a0dd64a050274d3f011170
Unknown <echa232@aucklanduni.ac.nz>
2017-08-07 18:07:45 +1200

    Renamed PartialSchedule class to Schedule

--- src/main/java/se306/team7/Schedule.java
+++ src/main/java/se306/team7/Schedule.java
@@ -2,7 +2,7 @@ package se306.team7;
 
 import se306.team7.Digraph.Node;
 
-public class PartialSchedule {
+public class Schedule {
 
     public int _numOfProcessors;
 
@@ -10,7 +10,7 @@ public class PartialSchedule {
      * Instantiates a PartialSchedule instance.
      * @param numOfProcessors the number of processors available for tasks to be scheduled on
      */
-    public PartialSchedule(int numOfProcessors) {
+    public Schedule(int numOfProcessors) {
         _numOfProcessors = numOfProcessors;
     }
 
