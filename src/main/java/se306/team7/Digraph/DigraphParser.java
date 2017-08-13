a7d1fa73a42f58c1556883f9a6d03a60af26a58c
Nathan Kear <n.j.kear@gmail.com>
2017-08-13 23:22:23 +1200

    Catch and inform user of unparsable lines in input

--- src/main/java/se306/team7/Digraph/DigraphParser.java
+++ src/main/java/se306/team7/Digraph/DigraphParser.java
@@ -1,6 +1,9 @@
 package se306.team7.Digraph;
 
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import se306.team7.TaskScheduler;
 import se306.team7.utility.IFileUtilities;
 
 import java.io.BufferedReader;
@@ -9,6 +12,7 @@ import java.io.IOException;
 
 public class DigraphParser implements IDigraphParser {
     IFileUtilities _fileUtilities;
+    private static Logger logger = LoggerFactory.getLogger(DigraphParser.class);
 
     /**
      * Instantiates an instance of DigraphParser
@@ -63,12 +67,16 @@ public class DigraphParser implements IDigraphParser {
      */
     private IDigraphBuilder parseLine(IDigraphBuilder db, String line){
 
-        line = line.trim();
-        String[] splitLine = line.split("\\s+");
-        if(splitLine.length<3){
-            db.addNode(splitLine[0], Integer.parseInt(splitLine[1].replaceAll("[^0-9]", "")));
-        }else{
-            db.addLink(splitLine[0], splitLine[2],Integer.parseInt(splitLine[3].replaceAll("[^0-9]", "")));
+        try {
+            line = line.trim();
+            String[] splitLine = line.split("\\s+");
+            if (splitLine.length < 3) {
+                db.addNode(splitLine[0], Integer.parseInt(splitLine[1].replaceAll("[^0-9]", "")));
+            } else {
+                db.addLink(splitLine[0], splitLine[2], Integer.parseInt(splitLine[3].replaceAll("[^0-9]", "")));
+            }
+        } catch (Exception ex) {
+            logger.warn("Unable to parse line: " + line);
         }
 
         return db;
