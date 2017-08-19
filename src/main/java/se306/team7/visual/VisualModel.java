8cfecf46657d1b09d843fddb8fe02a291e6fb7a7
Chen Li <cli727@aucklanduni.ac.nz>
2017-08-19 14:55:49 +1200

    implemented constructor for VisualModel

--- src/main/java/se306/team7/visual/VisualModel.java
+++ src/main/java/se306/team7/visual/VisualModel.java
@@ -1,4 +1,16 @@
 package se306.team7.visual;
 
 public class VisualModel {
+	
+	 private View_CurrentBest _view_currentBest;
+	 private View_Histogram	_view_histogram;
+	 private View_LineGraph _view_lineGraph;
+	 
+	 public VisualModel(){
+		 _view_currentBest = View_CurrentBest.getInstance();
+		 _view_histogram = View_Histogram.getInstance();
+		 _view_lineGraph = View_LineGraph.getInstance(); 
+	 }
+	 
+	 
 }
