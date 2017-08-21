550065a76a025ab83d6355d526432f8a09c2ff89
Emily Chan <echa232@aucklanduni.ac.nz>
2017-08-21 21:40:00 +1200

    Add range-recalibration capabilities for X-axis of View_LineGraph

--- src/main/java/se306/team7/visual/View_LineGraph.java
+++ src/main/java/se306/team7/visual/View_LineGraph.java
@@ -13,17 +13,19 @@ public class View_LineGraph implements ITaskSchedulerView {
     public LineChart<Number,Number> _lineChart;
     private static View_LineGraph _view_lineGraph;
     private static int currentTimeUnit = 0;
+    private NumberAxis _xAxis;
 
     private View_LineGraph() {
         //defining the axes
-        NumberAxis xAxis = new NumberAxis();
-        xAxis.setLabel("Time");
+        _xAxis = new NumberAxis();
+        _xAxis.setLabel("Time");
+        _xAxis.setAutoRanging(false);
 //        NumberAxis yAxis = new NumberAxis(1, Metrics.getLevels(), 1); //TODO use this line after Metrics class has been instantiated
         NumberAxis yAxis = new NumberAxis(1, 10, 1);
         yAxis.setLabel("Current level");
 
         //creating the chart
-        _lineChart = new LineChart<Number,Number>(xAxis, yAxis);
+        _lineChart = new LineChart<Number,Number>(_xAxis, yAxis);
         _lineChart.setTitle("Current level explored for each core");
         _lineChart.setAnimated(true);
         _lineChart.setLegendVisible(false);
@@ -55,6 +57,11 @@ public class View_LineGraph implements ITaskSchedulerView {
      */
     public void update(int numOfLevels, int numOfCores, int currentBestCost, HashMap<Integer, Integer> histogram, HashMap<Integer, Integer> coreCurrentLevel) {
         currentTimeUnit++;
+        double currentXAxisUpperBound = _xAxis.getUpperBound();
+        if (currentTimeUnit > (currentXAxisUpperBound * 0.8)) {
+            _xAxis.setUpperBound(currentXAxisUpperBound * 2);
+        }
+
         for (Map.Entry<Integer, Integer> entry : coreCurrentLevel.entrySet()) {
             _lineChart.getData().get(entry.getKey()-1) //get the series for the corresponding core
                     .getData().add(new XYChart.Data<Number, Number>(currentTimeUnit, entry.getValue())); //add new Y value for the current time unit
